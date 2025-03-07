9710360b8a9c639d4cc7e14b94670c8ebb6cdb76
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-07 12:13:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+document.querySelector("#form").addEventListener("submit",(e) => {
+	e.preventDefault();
+	const name = document.querySelector("#name").value;
+	const age = parseInt(document.querySelector("#age").value);
+	if(!name || isNaN(age)){
+		alert("Please enter valid details");
+		return;
+	}
+
+	const promise = new Promise((resolve,reject) => {
+		setTimeout(()=>{
+			if(age>18){
+				resolve(`Welcome,${name}. You can vote`);
+			}
+			else{
+				reject(`Oh sorry. You aren't old enough`);
+			}
+		},4000);
+    });
+	promise.then((data)=>{
+		alert(data);
+	}).catch((err)=>{alert(err)});
+})
